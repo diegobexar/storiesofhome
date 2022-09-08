c84634e501190dcbe63ef6e71219d2669883a47e
Diego Bejarano <bexar@uw.edu>
2022-09-08 10:42:54 -0600

    fixes footer content

--- footer.php
+++ footer.php
@@ -9,19 +9,24 @@
             <li class="widget widget_text">
                 <h4 class="widgettitle">Participate in STORIES OF HOME</h4>
                 <div class="textwidget">
-                    <p>Want to tell your story of finding home in Missoula?</p>
-                    <p>Let us know by <a href="">filling out a short survey.</a></p>
+                <p>Are you an immigrant or refugee who would like to share your story of finding a home in Missoula?</p>
+                    <p>Let us know by <a href="https://docs.google.com/forms/d/e/1FAIpQLSfZ8yE0skOw67MFjlWPAKeSf6ISbLvTi4jPFbFYf0umHKZhvw/viewform" target="_blank">filling out a short survey.</a></p>
                 </div>
             </li>
             <li class="widget widget_recent_entries">
-                <h4 class="widgettitle">SLM MISSION</h4>
-                <p>Brief mission statement</p>
+                <h4 class="widgettitle">OUR MISSION</h4>
+                <p><strong>Soft Landing Missoula</strong> strives to help Missoula, Montana be a welcoming, supportive and informed community that can assist refugees and immigrants to integrate and thrive.</p>
+                <p>Support SLM with a <a href="https://softlandingmissoula.org/donate" target="_blank" style="border-bottom:none;">donation</a></p>
             </li>
             <li class="widget widget_tag_cloud">
                 <h4 class="widgettitle">SOCIAL</h4>
                 <div class="tagcloud">
                     <a href="https://www.facebook.com/softlandingmissoula" target="_blank">Facebook</a>
                     <a href="https://www.instagram.com/softlandingmissoula/" target="_blank">Instagram</a>
+                </div>
+                <h4 class="widgettitle">PHOTOS</h4>
+                <div class="tagcloud">
+                    <p>Photographs by Helen Rolston-Clemmer</p>
                 </div>
             </li>
         </ul>
