c20e43287eab78c2d33aa0d6d50a5ecc02e2d9c8
Diego Bejarano <bexar@uw.edu>
2022-09-08 11:25:25 -0600

    revises index

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 
 <!DOCTYPE HTML>
 <html lang="en-US">
-<?php
+  <?php
 
-  $PageTitle = 'STORIES OF HOME MISSOULA 2021';
+    $PageTitle = 'STORIES OF HOME MISSOULA';
 
-?>
-<?php include 'header.php'; ?>
-
-
-    <!-- menu.php here -->
-    <?php include 'menu.php'; ?>
+  ?>
+    <?php include 'header.php'; ?>
 
     <?php include 'content-2021.php'; ?>
 
-    <!--Footer-->
     <?php include 'footer.php'; ?>
   </body>
 </html>
