380167710b3592c17451267556ed1ef19e7e7e45
Diego Bejarano <bexar@uw.edu>
2022-09-08 11:00:18 -0600

    updates header refs and brings menu code

--- header.php
+++ header.php
@@ -3,18 +3,19 @@
 <head>
   <title><?php echo $PageTitle ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="description" content="Soft Landing Missoula" />
-  <meta name="keywords" content="HTML, CSS, JavaScript, PHP" />
-  <meta name="author" content="SLM" />
+  <meta name="description" content="Soft Landing Missoula">
+  <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
+  <meta name="author" content="SLM">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-  <link rel="shortcut icon" href="/favicon.ico" />
-  <link href='https://fonts.googleapis.com/css?family=Playfair+Display%7CMontserrat:400,700%7CRoboto:300,300i,400,400i,700,700i' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display%7CMontserrat:400,700%7CRoboto:300,300i,400,400i,700,700i" rel="stylesheet" type="text/css">
 
-  <link rel="stylesheet" type="text/css" href='/css/clear.css' />
-  <link rel="stylesheet" type="text/css" href='/css/slick.css' />
-  <link rel="stylesheet" type="text/css" href='/css/common.css' />
-  <link rel="stylesheet" type="text/css" href='/css/sm-clean.css' />
+  <link rel="stylesheet" type="text/css" href="/css/clear.css">
+  <link rel="stylesheet" type="text/css" href="/css/slick.css">
+  <link rel="stylesheet" type="text/css" href="/css/common.css">
+  <link rel="stylesheet" type="text/css" href="/css/sm-clean.css">
+  <link rel="stylesheet" href="https://use.typekit.net/ymg6eyi.css">
   <link type="text/css" href="/css/style.css" rel="stylesheet">
 
 
@@ -35,3 +36,26 @@
 <body class="single single-story">
 
   <div class="doc-loader"></div>
+  <div class="header-holder center-relative relative content-1140">
+    <div class="header-logo center-text">
+        <a href="/">
+            <img src="../images/SLM_Logo.png" alt="Soft Landing Missoula logo">
+        </a>
+    </div>
+
+    <!--<div class="toggle-holder absolute">
+        <div id="toggle">
+            <div class="first-menu-line"></div>
+            <div class="second-menu-line"></div>
+            <div class="third-menu-line"></div>
+        </div>
+    </div>-->
+    <div class="clear"></div>
+  </div>
+  <div class="content-1140 header-content center-relative block">
+      <h1 class="entry-title">
+          STORIES OF HOME <br>
+          MISSOULA
+      </h1>
+      <div class="sub-head">Exploring the experiences of refugees and immigrants in Missoula, Montana</div>
+  </div>
