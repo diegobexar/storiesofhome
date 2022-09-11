473e3ef2c576f650c02533ac8adc838f56e58dec
Diego Bejarano <bexar@uw.edu>
2022-09-11 13:06:25 -0600

    adds initial content to index

--- index.php
+++ index.php
@@ -7,6 +7,51 @@
   ?>
     <?php include 'header.php'; ?>
     <?php include 'menu.php'; ?>
-    <?php include '2022/content-2022.php'; ?>
+
+        <div id="content" class="site-content grid text-start lh-base" style="width:970px;">
+            <h2>Where do you call home?</h2>
+            <p>It's an innocuous question, often asked as part of small talk or a harmless ice breaker. But for those who have been displaced or forced to uproot their lives -- like many of the refugees and immigrants we who have found their way to Missoula -- the answer is far from simple.</p>
+            <p>Put together by <a href="https://softlandingmissoula.org">Soft Landing Missoula</a>, <strong>Stories of Home</strong> explores the multitude of ways people define home, particularly when uncertainty prevails: the homes left behind and the new homes created; the homes we hope to create and those we only remember; the homes we find in a physical structure, and the homes we find in the comfort of a loved one.</p> 
+            <p>These multimedia projects pair rich photography with the spoken word and the written word to share the stories of those who arrived in Missoula as refugees and immigrants. The projects aim to elevate and amplify their voices as well demonstrate the shared experiences that mark our lives -- no matter where we call home.</p> 
+
+
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card text-bg-dark">
+                    <img src="/2021/images/charly-card.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title"><a href="/2021/">Stories of Home 2021</a></h3>
+                            <p class="card-text">
+                                <ul class="list-inline">
+                                    <li class="list-inline-item"><a href="/2021/charly/">CHARLY</a></li>
+                                    <li class="list-inline-item"><a href="/2021/tenzin/">TENZIN</a></li>
+                                    <li class="list-inline-item"><a href="/2021/zohair/">ZOHAIR</a></li>
+                                    <li class="list-inline-item"><a href="/2021/nereyda/">NEREYDA</a></li>
+                                    <li class="list-inline-item"><a href="/2021/paul/">PAUL</a></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-bg-dark">
+                    <img src="/2022/images/participant1-card-1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"><a href="/2022/">Stories of Home 2022</a></h3>
+                        <p class="card-text">
+                            <ul class="list-inline">
+                                <li class="list-inline-item text-uppercase"><a href="/2022/participant1/">participant1</a></li>
+                                <li class="list-inline-item text-uppercase"><a href="/2022/chanda/">CHANDA</a></li>
+                                <li class="list-inline-item text-uppercase"><a href="/2022/anseel/">ANSEEL</a></li>
+                                <li class="list-inline-item text-uppercase"><a href="/2022/participant2/">participant2</a></li>
+                                <li class="list-inline-item text-uppercase"><a href="/2022/participant3/">participant3</a></li>
+                            </ul>
+                        </p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     <?php include 'footer.php'; ?>
 </html>
