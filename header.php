3d192d295d1f9c7e4ef61a92dcaa2b10460951c0
Diego Bejarano <bexar@uw.edu>
2022-09-11 13:05:19 -0600

    adds bootstrap

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
   <link rel="stylesheet" type="text/css" href="/css/common.css">
   <link rel="stylesheet" type="text/css" href="/css/sm-clean.css">
   <link rel="stylesheet" href="https://use.typekit.net/ymg6eyi.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <link type="text/css" href="/css/style.css" rel="stylesheet">
 
 
