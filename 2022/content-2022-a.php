5f96aad04099475474ec1069e732f2426057f52b
Diego Bejarano <bexar@uw.edu>
2022-09-12 11:57:03 -0600

    test file for 2022 landing

---
+++ 2022/content-2022-a.php
@@ -0,0 +1,118 @@
+<?php
+
+  $title = 'STORIES OF HOME MISSOULA 2022';
+
+?>
+
+<div id="content" class="site-content">
+            <ul class="grid" id="story">
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2022/<?php echo $P1L; ?>/">
+                            <img src="images/<?php echo $P1L; ?>-1.jpg" alt="<?php echo $P1; ?>" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info">
+                                <a class="story-text text-uppercase" href="/2022/<?php echo $P1L; ?>/"><?php echo $P1; ?></a>
+                                <p class="story-category">
+                                    <a title="Afghanistan">Afghanistan</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2022/<?php echo $P1L; ?>/">
+                                        <img class="main-arrow" src="/assets/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2022/<?php echo $P2; ?>/">
+                            <img src="images/<?php echo $P2; ?>-1.jpg" alt="<?php echo $P2; ?>" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info">
+                                <a class="story-text text-uppercase" href="/2022/<?php echo $P2; ?>/"><?php echo $P2; ?></a>
+                                <p class="story-category">
+                                <a title="Democratic Republic of Congo, Rwanda">Democratic Republic of Congo<br /> Rwanda</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2022/<?php echo $P2; ?>/">
+                                        <img class="main-arrow" src="/assets/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2022/<?php echo $P3; ?>/">
+                            <img src="images/<?php echo $P3; ?>-1.jpg" alt="<?php echo $P3; ?>" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info">
+                                <a class="story-text text-uppercase" href="/2022/<?php echo $P3; ?>/"><?php echo $P3; ?></a>
+                                <p class="story-category">
+                                <a title="Democratic Republic of Congo, Rwanda">Democratic Republic of Congo<br /> Burundi</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2022/<?php echo $P3; ?>/">
+                                        <img class="main-arrow" src="/assets/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="grid-item element-item quote-item animate show-it">
+                    Quote here
+                </li>
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2022/<?php echo $P4; ?>/">
+                            <img src="images/<?php echo $P4; ?>-1.jpg" alt="<?php echo $P4; ?>" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info">
+                                <a class="story-text text-uppercase" href="/2022/<?php echo $P4; ?>/"><?php echo $P4; ?></a>
+                                <p class="story-category">
+                                    <a title="Democratic Republic of Congo">Democratic Republic of Congo</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2022/<?php echo $P4; ?>/">
+                                        <img class="main-arrow" src="/assets/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2022/<?php echo $P5; ?>/">
+                            <img src="images/<?php echo $P5; ?>-1.jpg" alt="<?php echo $P5; ?>" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info" style="margin-top:-100px";>
+                                <a class="story-text text-uppercase" href="/2022/<?php echo $P5; ?>/"><?php echo $P5; ?></a>
+                                <p class="story-category">
+                                    <a title="Afghanistan">Afghanistan</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2022/<?php echo $P5; ?>/">
+                                        <img class="main-arrow" src="/assets/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="clear"></div>
+             <!--<div class="block center-text load-more-story">
+                <a target="_self" class="more-posts-story">Load More</a>
+            </div> -->
+            <div class="clear"></div>
+        </div>
