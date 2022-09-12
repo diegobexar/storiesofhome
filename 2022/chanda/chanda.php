eeccb6963e354f426c4d50c769c4e575cdca6413
Diego Bejarano <bexar@uw.edu>
2022-09-12 17:56:55 -0600

    adds chanda

---
+++ 2022/chanda/chanda.php
@@ -0,0 +1,35 @@
+<div class="card text-bg-river border border-0">
+    <div class="card-body">
+    <p class="lead">Chanda truly believes she was meant to end up right here in Missoula, Montana. So much so, in fact, that she named one of her kids in honor of the arduous journey.</p> 
+
+    <p>“We are not here by accident, but we are here by the purpose of God,” Chanda recalls thinking when she received news of the delay. “My baby gave me purpose, and he is Purpose.”</p> 
+
+    <p>Chanda fled the bloody war in the Democratic Republic of the Congo when she was eight-years-old. She experienced violence first hand – her father had been kidnapped and her older brother had been killed. Her family first escaped to Zambia, and then Chanda went with one of her sisters to Malawi.</p> 
+
+    <p>“Things were so difficult. We struggled, and we started doing a lot of stuff just to survive – to have food and shelter,” Chanda says. </p>
+
+    <p>Years passed, and Chanda met her now-husband and had two kids as they awaited their approval to be resettled as refugees outside of Africa. When they got word that they had been approved for resettlement in the United States, Chanda found out she was pregnant with her third child – the baby who would be named Purpose – so she was unable to go through the extensive requisite medical exams, such as x-rays and imaging, due to potential risks to her pregnancy. Chanda and her family were told to wait yet again, and it would be years before their next chance.</p> 
+
+    <p>Almost two years after Purpose was born, the family received word that they would be resettled to Missoula, Montana. It was 2018. She first applied in 2014.</p> 
+
+    <p>“I’ve never heard of this place. Never. Some people said it was Missouri. Do we have Black people there? I don’t know,” Chanda recalls thinking. “So we said ‘okay, God, if you want us to go somewhere where there are no Black people then let us go.’” </p>
+
+    <p>When the family arrived at the airport, they were greeted by fellow Congolese refugees. Chanda felt immediate relief, and her family was quickly able to get started building a life.</p>
+
+    <p>She started working at a local hospital as a cleaner while her husband, Justin, worked at Albertsons on the night shift. Today, he is at Mountain Line as a bus driver and she is at the International Rescue Committee where she helps other refugees access healthcare. </p>
+
+    <p>Though Chanda was connected with local Congolese families, loneliness still crept in – exacerbated during her pregnancy with their fourth child and after she gave birth to baby Joyce.</p> 
+
+    <p>“Life in America can be difficult, especially for us who are used to being surrounded by people,” Chanda says. “In Africa, I used to have my babies but you are surrounded by help and people come to take care of you for two or more months. But, for this one, I was crying almost every day with my baby because I didn’t know what to do alone.”</p>
+
+    <p>And while there have been many major moments of transitions such as this one, Chanda remains steadfast in love of Missoula and gratitude for the life her family has been able to build. They even bought a house in Lolo in July, a sign of their commitment to this place.</p> 
+
+    <p>When asked how she defines ‘home,’ Chanda says she always thinks of the refugee camp where she spent many years after leaving the Congo. “I know my house, it was so small, but every day in the morning people would come and ask about the good news we have. We received visitors, cooked and shared,” she says. “That’s why I want to be surrounded by people, working with people. I don’t want to be alone because I’m used to having a lot of noise.” </p>
+
+    <p>But Missoula is also home, the place where she can send her kids to get a good education, make new friends and buy a house. It’s also a place where they are free to practice their traditions and their cultures – like speaking Swahili, putting on traditional dresses to attend church every Sunday and coming together for major Congolese holidays with other community members over the course of the year. </p>
+
+    <p>“We are moving forward, and we just want to live the American life. That doesn’t mean I need to forget about my culture,” Chanda says. “No, I cannot forget about my culture or where I’m from. I’m from Africa. I am African.” </p>
+
+
+</div>
+</div>
