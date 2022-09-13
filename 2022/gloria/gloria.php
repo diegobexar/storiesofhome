c022535bc6c018696e80c98be18baf91483a3e05
Diego Bejarano <bexar@uw.edu>
2022-09-13 06:54:50 -0600

    adds gloria photos to text

--- 2022/gloria/gloria.php
+++ 2022/gloria/gloria.php
@@ -4,27 +4,27 @@
 <p class="lead">When Gloria walked through the doors of Big Sky High School last month, she was the only refugee student enrolled in the entire school. Only 15-years-old, Gloria is wise beyond her years – she says she thought often throughout the summer about how she might stand out in the hallways. </p>
 
 <p>“I was getting a little bit nervous. I was mostly nervous about being at a school where there are not a lot of my friends, not a lot of other African people – or just Black people, in general,” she says. “I felt like it would be hard for me.” </p>
-
+<img src="/2022/images/gloria-6.jpg" class="mb-3" title="Gloria" />
 <p>But that nervousness does not mean she wants to hide her African culture. If anything, Gloria thinks it’s important that she embrace it and use her own experience as a way to help students and teachers understand some of the misconceptions they might have about her home. </p>
 
 <p>Gloria and her family arrived in Missoula in November 2019 from the Democratic Republic of the Congo by way of years in Rwanda. She was in middle school and eager to assimilate.</p>
-
+<img src="/2022/images/gloria-4.jpg" class="mb-3" title="Gloria" />
 <p>“I wanted to fit in, and I completely forgot anything &nbsp;about Africa and that caused me to forget one of my languages,” she recalls. “I lived in Rwanda for half of my life, and I forgot that language. It made me really mad that I was focusing more on English and I forgot that language, a language that was important to me.” </p>
 
 <p>Vowing to always remember where she came from, Gloria plans to bring African food to school for lunch. She’s excited to share those meals with classmates so they can get a little taste of her home. </p>
-
+<img src="/2022/images/gloria-2.jpg" class="mb-3" title="Gloria" />
 <p>“Ugali is going to be a part of me forever,” she says. “We’re tasting something from Africa, something from home.” </p>
 
 <p>Gloria also hopes to help dispel some common narratives around Africa. </p>
 
 <p>“It gets really frustrating when you see a lot of bad stuff about Africa, so I feel like I want to show them what it actually is – not only bad stuff, but lots of good, too,” she says. “You can’t just take one part of something and say that it’s everywhere.” </p>
-
-<p>Gloria will be a student in Big Sky’s Health Science Academy, an academically rigorous program that offers high schoolers particularly interested in a future in the field of STEM – shorthand for Science, Technology, Engineering and Mathematics – the chance to take more of these classes and benefit from mentorship in these spaces.Gloria thinks she wants to be a doctor or a nurse, so this curriculum might “get a step up” in that process when the time comes to think about college. </p>
+<img src="/2022/images/gloria-3.jpg" class="mb-3" title="Gloria" />
+<p>Gloria will be a student in Big Sky’s Health Science Academy, an academically rigorous program that offers high schoolers particularly interested in a future in the field of STEM – shorthand for Science, Technology, Engineering and Mathematics – the chance to take more of these classes and benefit from mentorship in these spaces. Gloria thinks she wants to be a doctor or a nurse, so this curriculum might “get a step up” in that process when the time comes to think about college. </p>
 
 <p>Taking such technical coursework in her non-native language has given Gloria some reason for pause. After three years in the United States, Gloria’s quite comfortable speaking English, but she’s worried teachers might equate that conversational &nbsp;fluency with an ability to read and write at the same level. </p>
 
 <p>Shopping for new clothes just before the beginning of the school year, Gloria talks about how she’s mostly just excited to get into her new routine and get settled into the new school. </p>
-
+<img src="/2022/images/gloria-5.jpg" class="mb-3" title="Gloria" />
 <p>Moving to Missoula introduced her to “stuff that I didn’t know I like until I tried” such as volleyball or track, and she’s thinking about trying out for those teams. She wants to speak with all of her teachers personally about her situation and try to figure out strategies for how to make sure she’s keeping up in classes. Gloria says she’s really excited “ to just experience new things and get to learn more about myself” over the next four years. </p>
 
 <p>Above all, she knows she’s entering this next chapter with a stronger sense of who she is and the importance of her African identity as part of that, and Gloria wants to pass along that message to other youth in the community who might have experienced something similar. </p>
