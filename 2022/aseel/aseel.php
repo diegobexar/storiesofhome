46b4a4994457d238ee66435fdbc6ad67898f6fb0
Diego Bejarano <bexar@uw.edu>
2022-09-13 06:28:51 -0600

    aseel images and text

--- 2022/aseel/aseel.php
+++ 2022/aseel/aseel.php
@@ -6,28 +6,30 @@
         <p class="lead">Aseel’s last image of Afghanistan is one of total chaos. </p>
 
         <p>As the Taliban’s takeover of Kabul became inevitable last summer, Aseel made the dangerous and chaotic journey with his wife and four children to the airport. He recalls throngs of people trying to escape and his family weaving through the crowds with a United States military escort in order to get on his flight out of the country. His mother, in her seventies, did not want him to go – but as a former member of the Ministry of Foreign Affairs under previous Afghan presidential administrations, it was clear to Aseel that he would be a target for the Taliban. </p>
-
+        <img src="/2022/images/aseel-1.jpg" class="mb-3" title="Aseel" />
         <p>Much of Aseel’s career had been spent trying to build a new future for Afghanistan: a more democratic system of governance; more robust economic opportunity; a place in the international community; and access to basic needs such as healthcare, food and education. </p>
 
         <p>Almost overnight, he watched as all of that crumbled. </p>
 
-       
+      <img src="/2022/images/aseel-3.jpg" class="mb-3" title="Aseel" />
         <p>“We worked to rebuild our country and to help because our country experienced a lot of war,” Aseel says. “We lost all those efforts in a matter of weeks. We lost everything, so I miss it all.” </p>
         
         <p>Sure enough, less than one week after Aseel and his immediate family fled Afghanistan, the Taliban raided his home and seized government-issued weapons, his vehicle and many personal belongings they had been forced to leave behind. Aseel says his mother then understood the grave danger he faced from the Taliban, but he now worries he might never be able to see her again should he be barred from returning to his home country of Afghanistan. </p>
 
         <p>Aseel is now focused on making Missoula feel like home. </p>
 
+        <img src="/2022/images/aseel-2.jpg" class="mb-3" title="Aseel" />
+
         <p>He’s driven by a strong call to service, the same one that motivated him to join the government in Afghanistan. “This is our home now, so we should work for this community,” Aseel says. “The beauty of Missoula will be the beauty of us.” </p>
 
         <p>But figuring out how to make that difference can be a challenge, Aseel says. </p>
 
         <p>Not very long ago, Aseel traveled the world as a diplomat, helped to craft policy, and sat at tables with powerful government officials from around the globe. He wants to use that background and those skills to make an impact in Missoula, but he’s felt limited by the opportunities – and people’s expectations of him as an Afghan evacuee – since his arrival. </p>
-
+        <img src="/2022/images/aseel-7.jpg" class="mb-3" title="Aseel" />
         <p>“We need to have good jobs, and I need to support my family,” he says. “But I also want to share my experience because I had a rich background and a lot of things to share.” </p>
 
         <p>Aseel recently enrolled in a Computer Science program at Missoula College, a decision he thinks will give him more skills to perhaps secure a higher paying job. He’s also been focused on getting to know Missoula, support the other Afghan arrivals and connect with new neighbors. He plays volleyball multiple times each week – and those teammates have quickly become friends who have shown Aseel and other Afghan players the city – and he organizes outings such as trips to the pool or hikes up to the M for the Afghan community through a WhatsApp thread. </p>
-
+        <img src="/2022/images/aseel-6.jpg" class="mb-3" title="Aseel" />
         <p>“The people here are not as social – they have their individual life, whereas in Afghanistan we study together, play together and work together,” Aseel says. “If you don’t talk to somebody, nobody is ready to say ‘hello,’ but once you have the courage to participate in a gathering and you start talking, people here are very welcoming and very helpful.” &nbsp;</p>
 
     </div>
