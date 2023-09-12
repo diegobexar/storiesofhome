f481d91b8bdd0bc55cfa2e2aa3a7d7602b30ea44
Diego Bejarano <bexar@uw.edu>
2023-09-12 16:56:52 -0600

    updates header and content page for 2023

---
+++ 2023/content-2023.php
@@ -0,0 +1,76 @@
+<?php
+
+  $title = 'STORIES OF HOME MISSOULA 2023';
+
+?>
+
+<div id="content" class="site-content">
+    <div class="image-grid">
+        <div class="image-row"> 
+            <div class="image-column">
+                <!-- start esther -->
+                <div class="name-overlay-wrapper animate show-it">
+                    <a href="/2023/<?php echo $P3_23; ?>/"><img class="img-22" src="images/<?php echo $P3_23; ?>-5.jpg" alt="Photo of <?php echo $P3_23; ?>"></a>
+                    <div class="name-overlay-content">
+                        <div class="text-uppercase fs-3"><a href="/2023/<?php echo $P3_23; ?>/"><?php echo $P3_23; ?></a></div>
+                        <small class="text-uppercase">Haiti</small>
+                    </div>
+                </div>
+                <!-- start abeer --->
+                <div class="name-overlay-wrapper animate show-it">
+                    <a href="/2023/<?php echo $P4_23; ?>/"><img class="img-22" src="images/<?php echo $P4_23; ?>-4.jpg" alt="Photo of <?php echo $P4_23; ?>"></a>
+                    <div class="name-overlay-content">
+                        <div class="text-uppercase fs-3"><a href="/2023/<?php echo $P4_23; ?>/"><?php echo $P4_23; ?></a></div>
+                        <small class="text-uppercase">Yemen</small>
+                    </div>
+                </div>
+                <!-- end first column -->
+            </div>
+            <div class="image-column">
+                <!-- start jp and janveier -->
+                <div class="name-overlay-wrapper animate show-it">
+                    <a href="/2023/<?php echo $P1L_23; ?>/"><img class="img-22" src="images/<?php echo $P1L_23; ?>-7.jpg" alt="Photo of <?php echo $P1_23; ?>"></a>
+                    <div class="name-overlay-content">
+                        <div class="text-uppercase fs-3"><a href="/2023/<?php echo $P1L_23; ?>/"><?php echo $P1_23; ?></a></div>
+                        <small class="text-uppercase">Democratic Republic of Congo</small>
+                    </div>
+                </div>
+                <!-- start kamar -->
+                <div class="name-overlay-wrapper animate show-it">
+                    <a href="/2023/<?php echo $P2_23; ?>/"><img class="img-22" src="images/<?php echo $P2_23; ?>-3.jpg" alt="Photo of <?php echo $P2_23; ?>"></a>
+                    <div class="name-overlay-content">
+                        <div class="text-uppercase fs-3"><a href="/2023/<?php echo $P2_23; ?>/"><?php echo $P2_23; ?></a></div>
+                        <small class="text-uppercase">Syria</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- <ul class="grid" id="story">
+                <li class="grid-item element-item animate show-it">
+                    <div class="item-wrapper">
+                        <a href="/2021/charly/">
+                            <img src="images/charly.jpg" alt="Charly" />
+                        </a>
+                        <div class="story-text-holder">
+                            <div class="story-info">
+                                <a class="story-text" href="/2021/charly/">CHARLY</a>
+                                <p class="story-category">
+                                    <a title="Democratic Republic of Congo, Uganda">Democratic Republic of Congo<br />Uganda</a>
+                                </p>
+                                <p class="story-arrow">
+                                    <a href="/2021/charly/">
+                                        <img class="main-arrow" src="images/opta_arrow@2x.png" alt="">
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="clear"></div>-->
+             <!--<div class="block center-text load-more-story">
+                <a target="_self" class="more-posts-story">Load More</a>
+            </div> -->
+    <div class="clear"></div>
+</div>
