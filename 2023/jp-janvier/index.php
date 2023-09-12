409965068fca6ba3523251afb7b037cc5fd6008e
Diego Bejarano <bexar@uw.edu>
2023-09-12 16:57:36 -0600

    adds participant index pages

---
+++ 2023/jp-janvier/index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<!-- use this template for creating single participant page with audio or text and save: /year/participant/index.php -->
+<?php
+  $FirstName = 'JP & Janvier';
+  $FirstNameL = 'jp-janvier';
+  $PageTitle = 'STORIES OF HOME - '.$FirstName;
+  $Country = 'Democratic Republic of Congo<br />Uganda'; //Democratic Republic of Congo<br />Burundi
+
+
+?>
+<?php require '../../header.php'; ?>
+<?php require '../../menu.php'; ?>
+
+    <div id="content" class="site-content">
+        <div class="content-1140 center-relative bottom-100">
+            <div class="center-relative clear">
+                <div class="entry-content">
+                    <div class="top-content">
+                    </div>
+                    <div class="content-970 center-relative">
+                        <div class="story-item-info">
+                            <div class="item-info-content">
+                                <div class="info-code text-end"">
+                                    <h1 class="info-code-title text-uppercase"><?php echo $FirstName; ?></h1>
+                                    <p class="info-code-content long-country"><?php echo $Country; ?></p>
+                                </div>
+                              <!--  <div class="info-code ">
+                                    <p class="info-code-title"></p>
+                                    <p class="info-code-content"></p>
+                                </div> -->
+                            </div>
+                            <div class="single-photo"><img src="/2023/images/<?php echo $FirstNameL; ?>-8.jpg" /></div>
+                        </div>
+                        <div class="content-wrapper">
+                            <?php include ('' . $FirstNameL .'.php'); ?>
+                        </div>
+                        <div class="clear"></div>
+                        <p class="text-end fw-light fst-italic mt-2">Story by <strong>Carly Graf</strong>. Photos by <strong>Madeline Jorden</strong></p>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+
+    <!--Footer-->
+  <?php require '../../footer.php'; ?>
+</html>
