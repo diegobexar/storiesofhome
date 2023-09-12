e7b2431acc999f87939374831c9536fb009c40bd
Diego Bejarano <bexar@uw.edu>
2023-09-12 16:57:58 -0600

    adds participant content pages

---
+++ 2023/kamar/kamar.php
@@ -0,0 +1,50 @@
+<div class="card text-bg-river border border-0">
+    <div class="card-body">
+
+   <p class="lead"> Today, Syria is known by many across the world as a country scourged by civil war. Since 2011, more than 14 million people have been forced to flee the country in search of safety and refuge from a violent government, according to the UNHCR, and over 70 percent of people still living in Syria are in dire need of humanitarian assistance. </p>
+
+   <img src="/2023/images/kamar-2.jpg" class="mb-3" title="Kamar" />
+
+   <p>But ask Kamar, who grew up in Aleppo where some of the war’s worst fighting has been concentrated, and she’ll tell you that the Syria of today is not the Syria of her childhood. </p>
+
+   <p>“We didn’t know what war was, what war planes were, or what bombing was. We hadn’t seen that before in our lives, and we definitely didn’t see people getting killed right in front of us,” she said. “When the war started, the fear started.” </p>
+
+   <p>Kamar was studying to become a teacher when the war began. She and her husband Majd fled to Lebanon, where they would stay for nearly 10 years, leaving behind their families and friends. Kamar recalls feeling like it was a completely new life, even though the two countries border one another. She had to give up her studies in order to work to help earn money along with her husband. </p>
+
+   <img src="/2023/images/kamar-1.jpg" class="mb-3" title="Kamar" />
+
+   <p>It wasn’t easy. Kamar says being unable to see her family caused her great pain, and while they met many people in Lebanon who “stood with us,” they always knew it would be hard for them to build a life for themselves there due to the country’s political and economic instability. After nearly a decade waiting for resettlement as refugees, Kamar, Majd, and their two sons who had been born in Lebanon heard the news that they would be able to move to Missoula, Montana in April 2022. </p>
+
+   <img src="/2023/images/kamar-3.jpg" class="mb-3" title="Kamar" />
+
+   <p>“We had wanted to come to this country to start working for ourselves, to work to make things better to provide a better future for our kids,” she said. “I couldn’t believe we finally arrived after all that struggle.” </p>
+
+   <p>When the family arrived in Missoula, Kamar said she was nervous about whether people would be welcoming. She was “surprised” by how kind everyone was to her, and she was almost equally shocked that she enjoyed the cold winter weather and the mountains. But Kamar’s strength would be tested once again. Her youngest son, Jood, was diagnosed with spinal muscular atrophy not long after they arrived in the United States. His condition requires specialized medical care, regular physical therapy sessions, and a tremendous amount of hands-on attention from Kamar and Majd. </p>
+
+   <img src="/2023/images/kamar-4.jpg" class="mb-3" title="Kamar" />
+
+   <p>“I wasn’t expecting it to be this difficult, and it’s a new part of life for me and my son,” Kamar said. “I have to be strong for him.” </p>
+
+   <p>Today, Kamar and her family are working hard to make Missoula home, which she considers a place she can go to “forget about worries and all of these things that happen outside, a place where we can be comfortable.” </p>
+
+   <p>“For me, home is everything,” she said. </p>
+
+   <p>But it will take time. </p>
+
+   <p>Syria will always be her home. Kamar misses the gatherings at her grandmother’s house, where the entire family would gather for meals and to laugh together. “It’s a beautiful country, and people were kind when we were at peace. It’s true that we didn’t have a lot, but we were happy and we were safe and we never felt scared,” Kamar said. </p>
+
+   <p>And while Kamar hopes that one day she might be able to return to Syria to visit the family she left behind and the memories she holds dear, she also recognizes that Missoula can be home, too. “I came here so my home is here, and I’ll start to consider it as my home.” </p>
+
+   <p>Part of that process is planning – and dreaming – for the future. </p>
+
+   <img src="/2023/images/kamar-5.jpg" class="mb-3" title="Kamar" />
+
+   <p>Kamar makes handmade candles, a craft she learned from her mother back in Syria. She used to sit and watch as her mom would mold colorful wax into gorgeous creations, and Kamar recalls her mom having to help her “over and over” until she finally learned the skill. This year, Kamar started a business named after her youngest son – Jood Candles – that she operated at the Missoula People's Market. “My mom told me to do this as my career, so that’s what I’m trying to do right now,” Kamar said. </p> 
+
+   <p>The mother of two also dreams of a vibrant future in Missoula for her family. </p>
+
+   <p>“I wish for my youngest son to become independent. I would like him to feel better because he is sick,” Kamar said. “I want to learn English; I want my older son to learn and study what he loves; I want my husband to like what he does for work. We just want to feel comfortable.” </p>
+   
+
+    </div>
+</div>
