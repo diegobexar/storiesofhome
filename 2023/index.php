548d86a861056c692424bd4ecb77fe0d19826bbb
Diego Bejarano <bexar@uw.edu>
2023-09-12 16:58:06 -0600

    updates index for 2023

---
+++ 2023/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+  <?php
+
+    $PageTitle = 'STORIES OF HOME MISSOULA';
+
+  ?>
+    <?php include '../header.php'; ?>
+    <?php include '../menu.php'; ?>
+    <?php include 'content-2023.php'; ?>
+
+    <?php include '../footer.php'; ?>
+</html>
