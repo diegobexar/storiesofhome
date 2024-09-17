d262b9c944664030b7c7414a6ae85bc8e00c0a55
Diego Bejarano <bexar@uw.edu>
2024-09-17 08:10:32 -0600

    adds ange and all images

---
+++ 2024/ange/ange.php
@@ -0,0 +1,45 @@
+<div class="card text-bg-river border border-0">
+    <div class="card-body">
+
+<p class="lead">Ange's story begins with what home means to her. "Home is where you are at peace," she reflects. 
+    In her birthplace, the Democratic Republic of the Congo, Ange's life looked very different. When arriving in Missoula, she found her new environment to be welcoming and serene. 
+    "So when we got here, we got peace, and the people around us care so much. They make you feel loved and safe," Ange shares.</p>
+
+    <img src="/2023/images/ange-3.jpg" class="mb-3" title="Ange" />
+
+    <p>Although the transition to Missoula had its challenges, the presence of her family in Missoula transforms the unfamiliar into the familiar. 
+    Ange recalls the struggle with a new language and culture. "It was hard," she admits, the memory still vivid. The weather was different, the people unfamiliar, and the language barrier daunting. 
+    Yet, with determination and the support of organizations like the International Rescue Committee and Soft Landing Missoula, she navigated these obstacles, finding peace and a sense of relief in her new surroundings. 
+    These organizations provided not just practical assistance but also a sense of community and belonging.</p>
+
+    <p>Education became a beacon of hope for Ange. Arriving in 2019 at the age of 17, she embraced the opportunity to learn, despite the language barrier. 
+    The school environment was initially overwhelming, with its fast-paced English conversations and unfamiliar teaching methods. 
+    However, Ange's perseverance shone through. She set out to master the language, often staying up late to study and practice. 
+    Despite her being practically fluent in five languages, she exclaimed that English has been the hardest to learn.</p>
+
+    <p>Now a student at the University of Montana, Ange is on a path to becoming a nurse, driven by a desire to give back to the community that embraced her. 
+    "I want to help people because many helped me," she says. Her aspiration to become a nurse is fueled by the kindness she received and her innate desire to care for others. 
+    During the school year, Ange studies hard. She can often be seen in the Soft Landing community center, studying with volunteer tutor matches. 
+    One volunteer raves about how she excels in human biology, a notoriously difficult subject—even earning a 100% on her final exam in Anatomy and Physiology! 
+    The path to nursing school is not an easy one, but this is just a testament to how hardworking she is. Ange never gives up and knows she can achieve her dreams, balancing her roles as a CNA and a full-time student. 
+    After five years in the US, Ange is excited to start her path to citizenship. While this endeavor will require even more long days of studying, she looks forward to gaining citizenship—a step that will make the US feel even more like home.</p>
+
+    <p>Ange's connection to her roots remains strong. At home, she speaks her African languages, including Swahili and Luganda, and learns traditional cooking from her mother, ensuring that her cultural heritage is alive and vibrant. 
+    This connection is something she cherishes. Cooking traditional meals together is not just about preserving recipes; it is about sharing stories, memories, and quality time as a family.</p>
+
+    <p>The community in Missoula has been a source of strength for Ange. "We have a great community here," she shares. Through this network, she has forged friendships with people from diverse backgrounds. 
+    These relationships have provided her with emotional support and a sense of belonging, reinforcing the idea that home is not just a physical space but a feeling of connection, acceptance, and ultimately peace. 
+    "It's okay to feel like you're lost. But one day, you feel better," Ange advises new arrivals, encouraging them to trust the kindness of strangers, even when it feels unfamiliar.</p>
+
+    <p>Ange's message to new residents is one of hope and perseverance. She believes it's okay to start a new life, acknowledging that while it may be different and difficult at times, peace awaits. 
+    "You will meet good people, have a job, and have an opportunity to go to school," she encourages. 
+    Ange reflects on her own journey, recalling how in Uganda, she couldn't finish high school due to financial constraints. 
+    However, upon arriving in Missoula, she was able to continue her education, for which she's deeply grateful. 
+    "Keep believing in yourself and keep pushing forward," Ange advises. She emphasizes that everything is possible as long as one is alive, even if it's challenging. 
+    Ange believes that one day, newcomers will look back and see the significant difference in their lives and recognize their achievements. 
+    Her final words of wisdom resonate with determination: "Never give up on your dreams. Keep working hard and try to make a positive difference."</p>
+
+    <p>As Ange looks to the future, her dreams are clear. She envisions a life where she can continue to help others, inspired by the kindness she received. 
+    Her journey is a reminder that home is not just a place but a feeling of safety, love, and belonging.</p>
+    </div>
+</div>
